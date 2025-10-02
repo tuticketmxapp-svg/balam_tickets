d770efa7ec5eef726b025c369eadada9949cff11
Joel <troya4888pumas@gmail.com>
2025-10-02 17:37:27 -0600

    cambio de prueba

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('balam_tickets');
+  protected readonly title = signal('Joel');
 }
