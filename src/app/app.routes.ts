e40f4889c2f53bdec11fdecf7438ee578ad98853
Joel <troya4888pumas@gmail.com>
2025-10-07 01:28:53 -0600

    feat: navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', // 2. Cuando la ruta esté vacía (la raíz del sitio)
+    component: Home // 3. Muestra el HomeComponent
+  },
+];
