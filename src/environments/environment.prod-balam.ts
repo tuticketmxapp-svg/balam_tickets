91cb8a0c3d2490803feaa3fff854838a7d7bd4a9
Yazmin Fuentes <yazmin_1808@hotmail.com>
2025-10-28 07:03:16 -0600

    archivo de prod en environment

---
+++ src/environments/environment.prod-balam.ts
@@ -0,0 +1,34 @@
+export const environment = {
+  production: true,
+  digitalcertificate: '/assets/qz/digital-certificate.txt',
+  apiV1: 'https://api.balamtickets.com/v1/',
+  apiTicketsV1: 'https://api.balamtickets.com/v1/',
+  apiTickets: 'https://sandbox-tickets.accesototal.mx/api/',
+  pdf: 'https://accesototal.mx:3001/api/v1/',
+  secure: 'https://sandbox.accesototal.mx',
+  seatsIo: {
+    region: 'na', // e.g. "eu"
+    workspaceKey: 'eb78870b-22d3-4c24-91dd-4444140f55f7',
+    secretKey: '1552c487-8e99-4585-ade4-986713771216',
+    url: 'https://api-na.seatsio.net/',
+  },
+  OPENPAY_PREFIX: 'TT',
+  EMAIL_FROM_NAME: 'Tu Ticket Mx',
+  REPLY_EMAIL: 'no-reply@accesototal.mx',
+  firebaseConfig: {
+    apiKey: 'AIzaSyD_ZBpPTFnw3tXHh0kPAgyUoPotqdSRI5g',
+    authDomain: 'tuticketmx-86e25.firebaseapp.com',
+    projectId: 'tuticketmx-86e25',
+    storageBucket: 'tuticketmx-86e25.appspot.com',
+    messagingSenderId: '1025536490660',
+    appId: '1:1025536490660:web:76114706eba7c12bd0fa31',
+    measurementId: 'G-7192KRC9F3',
+  },
+  apiConekta: 'https://api.digitalfemsa.io/checkouts',
+  authorizationConekta: 'Bearer key_Dq5eX9uQi7SC6XUgCrsphZ7',
+  OpenPay: {
+    Id: 'mfva2emlrvqchkglt4zj',
+    ApiKey: 'pk_9b4f815d3bc24ca9bc2b3841672291fe',
+    SandboxMode: true,
+  },
+};
